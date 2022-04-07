7b830af9200a6041a0472f39fc1aecfbdac6c7a0
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-04-07 21:27:21 +0530

    day 1 assignment

--- unit_5_coding/week1/day1/ass/src/index.js
+++ unit_5_coding/week1/day1/ass/src/index.js
@@ -1,13 +1,20 @@
 
-import "../index.html"
+import  "../index.html" 
+import  "./index.css" 
+import logo from "../images/mern2.jpg"
+
+let image = document.createElement("img");
+image.setAttribute("class","photo");
+image.src = logo;
+document.querySelector("#logo").append(image);
 
 document.querySelector("form").addEventListener("submit", show);
 
 function show(event){
     event.preventDefault();
 
-    let text = document.getElementById("nootbook").value
-    // console.log(text);
+    let text = document.getElementById("note").value
+    console.log(text);
     let para = document.createElement("p");
     para.textContent = "";
     para.textContent = text;
